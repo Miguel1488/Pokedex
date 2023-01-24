906c83156a8a3de1fe3d5b11b2d442abd560055b
Miguel1488 <118023734+Miguel1488@users.noreply.github.com>
2023-01-23 23:32:31 -0500

    hjj

--- src/compones/Header.jsx
+++ src/compones/Header.jsx
@@ -4,6 +4,9 @@ const Header = () => {
     return (
         <div className='containerHeader'>
             <img src="./src/assets/imag/image 11.png" alt=""></img>
+            <body>
+                <img src="./src/assets/imag/fondo" alt="" />
+            </body>
          </div>
            
     );
